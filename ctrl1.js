7303227e60196b63ef32feff6b111bcb27f18b15
ksunair <ksunair@yahoo.com>
2016-01-12 22:52:55 -0600

    Added service

--- ctrl1.js
+++ ctrl1.js
@@ -1,7 +1,21 @@
 var myApp = angular.module('app',[]);
 
-myApp.controller('GreetingController', ['$scope', '$http', function($scope, $http) {
+myApp.service('userService', function ($http) {
+    var model = this;
+    var URLS = {
+        USER: 'http://localhost:8000/api/users'
+    };
+    model.getUsers = function () {
+        return $http.get(URLS.USER);
+    }
+});
+
+myApp.controller('GreetingController', ['$scope', '$http', 'userService', function($scope, $http, userService) {
     $http.get("http://localhost:8000/api/users").then(function(response) {
         $scope.users = response.data;
     });
+    userService.getUsers()
+        .then(function (result) {
+            $scope.users1 = result;
+        })
 }]);
