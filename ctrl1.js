e492813e01d794ff018781dc30c3b72d55128b5e
ksunair <ksunair@yahoo.com>
2016-01-14 22:43:28 -0600

    Added ui remote

--- ctrl1.js
+++ ctrl1.js
@@ -1,20 +1,20 @@
-var myApp = angular.module('app',[]);
-
-myApp.service('userService', function ($http) {
-    var URLS = {
-        USER: 'http://localhost:8000/api/users'
-    };
-    this.getUsers = function () {
-        return $http.get(URLS.USER);
-    }
-});
-
-myApp.controller('GreetingController', ['$scope', '$http', 'userService', function($scope, $http, userService) {
-    $http.get("http://localhost:8000/api/users").then(function(response) {
-        $scope.users = response.data;
-    });
-    userService.getUsers()
-        .then(function (result) {
-            $scope.users1 = result;
-        })
-}]);
+angular.module('app', [
+        'ngRoute',
+        'userControllers'
+    ])
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+            when('/users', {
+                templateUrl: 'users/templates/users.html',
+                controller: 'UsersCtrl'
+            }).
+            when('/users/:name', {
+                templateUrl: 'users/templates/userdetails.html',
+                controller: 'UserDetailsCtrl'
+            }).
+            otherwise({
+                redirectTo: '/users'
+            });
+        }
+    ]);
